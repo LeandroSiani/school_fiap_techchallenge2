adf2c2058d46058232e0bb4bf87ea800991ccae5
Lendro lombaldo <leandrosilva.hair@gmail.com>
2024-07-29 20:13:15 -0300

    Testes spec post

--- src/post/controllers/post.controller.spec.ts
+++ src/post/controllers/post.controller.spec.ts
@@ -1,18 +1,110 @@
+// post.controller.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
-import { PostControllerController } from './post.controller';
+import { PostController } from './post.controller';
+import { PostService } from '../services/post.service';
 
-describe('PostControllerController', () => {
-  let controller: PostControllerController;
+describe('PostController', () => {
+  let controller: PostController;
+  let service: PostService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [PostControllerController],
+      controllers: [PostController],
+      providers: [
+        {
+          provide: PostService,
+          useValue: {
+            findAllPublished: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            findByQuery: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            publish: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<PostControllerController>(PostControllerController);
+    controller = module.get<PostController>(PostController);
+    service = module.get<PostService>(PostService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAllPublished', () => {
+    it('deve chamar o método findAllPublished do serviço com os parâmetros limit e page corretos', async () => {
+      const limit = 10;
+      const page = 1;
+      await controller.findAllPublished(limit, page);
+      expect(service.findAllPublished).toHaveBeenCalledWith(limit, page);
+    });
+  });
+
+  describe('findAll', () => {
+    it('deve chamar o método findAll do serviço com os parâmetros limit e page corretos', async () => {
+      const limit = 10;
+      const page = 1;
+      await controller.findAll(limit, page);
+      expect(service.findAll).toHaveBeenCalledWith(limit, page);
+    });
+  });
+
+  describe('findOne', () => {
+    it('deve chamar o método findOne do serviço com o parâmetro id correto', async () => {
+      const id = 1;
+      await controller.findOne(id);
+      expect(service.findOne).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe('findByQuery', () => {
+    it('deve chamar o método findByQuery do serviço com a string de consulta correta', async () => {
+      const query = 'test';
+      await controller.findByQuery(query);
+      expect(service.findByQuery).toHaveBeenCalledWith(query);
+    });
+  });
+
+  describe('create', () => {
+    it('deve chamar o método create do serviço com os parâmetros corretos, incluindo data e valores padrão', async () => {
+      const dto = { title: 'Test', content: 'Content' };
+      await controller.create(dto);
+      expect(service.create).toHaveBeenCalledWith({
+        ...dto,
+        date: expect.any(Date),
+        isPublished: false,
+        publishDate: null,
+        id: 0,
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('deve chamar o método update do serviço com o id e os parâmetros de atualização corretos', async () => {
+      const dto = { title: 'Test', content: 'Content', isPublished: false, date: new Date(), publishDate: null };
+      const id = 1;
+      await controller.update(dto, id);
+      expect(service.update).toHaveBeenCalledWith(id, { id, ...dto });
+    });
+  });
+
+  describe('publish', () => {
+    it('deve chamar o método publish do serviço com o id correto', async () => {
+      const id = 1;
+      await controller.publish(id);
+      expect(service.publish).toHaveBeenCalledWith(id);
+    });
+  });
+
+  describe('remove', () => {
+    it('deve chamar o método remove do serviço com o id correto', async () => {
+      const id = 1;
+      await controller.remove(id);
+      expect(service.remove).toHaveBeenCalledWith(id);
+    });
   });
 });
--- src/post/services/post.service.spec.ts
+++ src/post/services/post.service.spec.ts
@@ -1,18 +1,105 @@
+// post.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostService } from './post.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from '../entities/post.entity';
 
 describe('PostService', () => {
   let service: PostService;
+  let repository: Repository<Post>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostService],
+      providers: [
+        PostService,
+        {
+          provide: getRepositoryToken(Post),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<PostService>(PostService);
+    repository = module.get<Repository<Post>>(getRepositoryToken(Post));
   });
 
-  it('should be defined', () => {
+  it('deve garantir que o serviço esteja definido', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('deve retornar um array de posts', async () => {
+      const result = [new Post()];
+      jest.spyOn(repository, 'find').mockResolvedValue(result);
+
+      expect(await service.findAll(10, 1)).toBe(result);
+    });
+  });
+
+  describe('findAllPublished', () => {
+    it('deve retornar um array de posts publicados', async () => {
+      const result = [new Post()];
+      jest.spyOn(repository, 'find').mockResolvedValue(result);
+
+      expect(await service.findAllPublished(10, 1)).toBe(result);
+    });
+  });
+
+  describe('findOne', () => {
+    it('deve retornar um post único', async () => {
+      const result = new Post();
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue(result);
+
+      expect(await service.findOne(1)).toBe(result);
+    });
+  });
+
+  describe('create', () => {
+    it('deve salvar um novo post', async () => {
+      const result = new Post();
+      jest.spyOn(repository, 'save').mockResolvedValue(result);
+
+      expect(await service.create(result)).toBe(result);
+    });
+  });
+
+  describe('update', () => {
+    it('deve atualizar um post existente', async () => {
+      const result = new Post();
+      jest.spyOn(repository, 'update').mockResolvedValue(undefined);
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue(result);
+
+      expect(await service.update(1, result)).toBe(result);
+    });
+  });
+
+  describe('remove', () => {
+    it('deve remover um post', async () => {
+      jest.spyOn(repository, 'delete').mockResolvedValue(undefined);
+
+      expect(await service.remove(1)).toBe(undefined);
+    });
+  });
+
+  describe('publish', () => {
+    it('deve publicar um post', async () => {
+      const result = new Post();
+      jest.spyOn(repository, 'findOneBy').mockResolvedValue(result);
+      jest.spyOn(repository, 'save').mockResolvedValue(result);
+
+      result.isPublished = true;
+
+      expect(await service.publish(1)).toBe(result);
+    });
+  });
+
+  describe('findByQuery', () => {
+    it('deve retornar posts que correspondem à consulta', async () => {
+      const result = [new Post()];
+      jest.spyOn(repository, 'find').mockResolvedValue(result);
+
+      expect(await service.findByQuery('test')).toBe(result);
+    });
   });
 });
